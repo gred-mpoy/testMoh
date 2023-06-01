99b8548f67298e77fd6df8485c5afff41d7a9528
gred-mpoy <46019853+gred-mpoy@users.noreply.github.com>
2023-06-01 23:26:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -183,7 +183,7 @@ st.image(image_url, use_column_width=True)
 st.title("Keyword prediction tool Stackoverflow ") 
 
 Title_input = st.text_input("Write the title of your request below")
-input_body_utilisateurs = st.sidebar.text_input("Enter the content of your request below ", height=200)
+input_body_utilisateurs = st.sidebar.text_input("Enter the content of your request below ")
 
 #reponse = applying(input_body_utilisateurs)
 
